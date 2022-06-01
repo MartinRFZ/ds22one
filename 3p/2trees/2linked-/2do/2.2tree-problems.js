52710f62787a5ba638ba73fe3f1d8d737095bb04
MartinRFZ <101826289+MartinRFZ@users.noreply.github.com>
2022-06-01 17:36:09 -0500

    Update 2.2tree-problems.js

--- 3p/2trees/2linked-/2do/2.2tree-problems.js
+++ 3p/2trees/2linked-/2do/2.2tree-problems.js
@@ -25,3 +25,44 @@
  * posorder(root)
  * 
  */
+function inorder(a){
+    if (a!==null){
+        inorder(a.l)
+        console.log(a.d)
+        inorder(a.r)
+    }
+}
+function postorder(a){
+    if (a!==null){
+        postorder(a.l)
+        postorder(a.r)
+        console.log(a.d)
+    }
+}
+function preorder(a){
+    if (a!==null){
+        console.log(a.d)
+        preorder(a.l)
+        preorder(a.r)
+    }
+}
+function Node(o){
+    this.d=o//bject<->data
+    this.l=null//eft
+    this.r=null//ight
+}
+let ob = new Node('+')
+let b1 = new Node('-')
+let b2 = new Node(3)
+let b3 = new Node(8)
+let b4 = new Node(3)
+ob.l = b1
+b1.l = b3
+b1.r = b4
+ob.r = b2
+console.log('Preorder:')
+console.log(preorder(ob))
+console.log('Inorder')
+console.log(inorder(ob))
+console.log('Postorder')
+console.log(postorder(ob))
